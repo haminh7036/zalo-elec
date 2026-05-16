4187526773a67576c2cdf59014ead247fd26037c
Minh Ha <minhha7036@gmail.com>
2026-05-16 14:57:35 +0700

    refactor: remove packaged environment path resolution in getResourcePath

--- src/main/index.ts
+++ src/main/index.ts
@@ -43,12 +43,6 @@ function getChromeUserAgent(): string {
  * @returns {string} The absolute path to the resource.
  */
 function getResourcePath(relativePath: string): string {
-    if (app.isPackaged) {
-        const cleanPath = relativePath.startsWith('resources/')
-            ? relativePath.replace('resources/', '')
-            : relativePath
-        return join(process.resourcesPath, cleanPath)
-    }
     return join(__dirname, '../../', relativePath)
 }
 
